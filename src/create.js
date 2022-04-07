500ff1df5eb615a9481634ad1d046a87a06de609
Matthew LaRocca <mlarocca.cpp@gmail.com>
2022-04-07 15:03:14 -0700

    Run create project script in submodule

--- src/create.js
+++ src/create.js
@@ -4,12 +4,14 @@ const {spawnSync} = require("child_process");
 
 const framework64Repo ="https://github.com/matthewcpp/framework64.git";
 
+let logStdOut = true;
+
 function create(name, projectDir, options) {
     if (fs.existsSync(projectDir)) {
         console.log(`framework64 project directory: ${projectDir}`);
     }
     else {
-        console.log(`Directory does not exist: ${projectDir}`);
+        console.error(`Directory does not exist: ${projectDir}`);
         process.exit(1);
     }
 
@@ -20,23 +22,28 @@ function create(name, projectDir, options) {
         fs.mkdirSync(libDir);
     }
 
+    const submoduleDir = path.join(libDir, "framework64");
+
     checkSpawnResult(spawnSync("git", ["submodule", "add", framework64Repo, "lib/framework64"], {cwd: projectDir}), "Add framework64 submodule");
     checkSpawnResult(spawnSync("git", ["submodule", "init"], {cwd: projectDir}), "Initialize framework64 submodule");
     checkSpawnResult(spawnSync("git", ["add", ".gitmodules"], {cwd: projectDir}), null);
 
     if (options.hasOwnProperty("branch")) {
-        const submoduleDir = path.join(libDir, "framework64");
-        checkSpawnResult(spawnSync("git", ["checkout", "-b", options.branch], {cwd: submoduleDir}), `Checkout framework64 branch: ${options.branch}`);
+        checkSpawnResult(spawnSync("git", ["checkout", options.branch], {cwd: submoduleDir}), `Checkout framework64 branch: ${options.branch}`);
         checkSpawnResult(spawnSync("git", ["add", "lib/framework64"], {cwd: projectDir}), null);
     }
 
     checkSpawnResult(spawnSync("git", ["commit", "-m", "Add framework64 submodule"], {cwd: projectDir}), "Create initial commit");
 
-
+    checkSpawnResult(spawnSync("npm", ["run", "--prefix", "lib/framework64", "create-game", name, projectDir], {cwd: projectDir}), `Create game: ${name}`);
 }
 
 
 function checkSpawnResult(result, description) {
+    if (logStdOut && result.stdout) {
+        console.log(result.stdout.toString());
+    }
+
     if (result.status === 0) {
         if (description)
             console.log(`${description}: OK`);
@@ -44,9 +51,11 @@ function checkSpawnResult(result, description) {
         return;
     }
 
-    console.log(`${description}: Error`);
+    if (description)
+        console.error(`${description}: Error`);
+    
     if (result.stderr && result.stderr.length > 0) 
-        console.log(result.stderr.toString());
+        console.error(result.stderr.toString());
 
     process.exit(1);
 }
